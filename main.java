1d22b518ceafafaa86952acf591ddfc547f6e96d
Haoran Li (Evan) <65979806+Haoranli802@users.noreply.github.com>
2024-02-23 13:54:30 +0800

    Update main.java

--- main.java
+++ main.java
@@ -17,3 +17,89 @@ class Solution {
 }
 
 // Time: O(N) Space: O(1) 虚拟节点
+
+//LeetCode 707
+
+class ListNode {
+    int val;
+    ListNode next;
+    ListNode(){}
+    ListNode(int val) {
+        this.val=val;
+    }
+}
+
+class MyLinkedList {
+
+    ListNode head;
+    int size;
+    public MyLinkedList() {
+        head = new ListNode(0);
+        size = 0;
+    }
+    
+    public int get(int index) {
+        if(index < 0 || index >= size) return -1;
+        ListNode temp = head;
+        for(int i = 0; i < index + 1; i++){
+            temp = temp.next;
+        }
+        return temp.val;
+    }
+    
+    public void addAtHead(int val) {
+        ListNode temp = new ListNode(val);
+        temp.next = head.next;
+        head.next = temp;
+        size++;
+    }
+    
+    public void addAtTail(int val) {
+        ListNode temp = head;
+        while(temp.next != null){
+            temp = temp.next;
+        }
+        temp.next = new ListNode(val);
+        size ++;
+    }
+    
+    public void addAtIndex(int index, int val) {
+        if(index < 0 || index > size) return;
+        else if(index == 0) addAtHead(val);
+        else if(index == size) addAtTail(val);
+        else{
+            ListNode temp = head;
+            for(int i = 0; i < index; i++){
+                temp = temp.next;
+            }
+            ListNode cur = new ListNode(val);
+            cur.next = temp.next;
+            temp.next = cur;
+            size ++;
+        }
+    }
+    
+    public void deleteAtIndex(int index) {
+        if(index < 0 || index >= size) return;
+        else if(size == 0) return;
+        else{
+            ListNode temp = head;
+            for(int i = 0; i < index; i++){
+                temp = temp.next;
+            }
+            temp.next = temp.next.next;
+        }
+        size --;
+    }
+}
+
+/**
+ * Your MyLinkedList object will be instantiated and called as such:
+ * MyLinkedList obj = new MyLinkedList();
+ * int param_1 = obj.get(index);
+ * obj.addAtHead(val);
+ * obj.addAtTail(val);
+ * obj.addAtIndex(index,val);
+ * obj.deleteAtIndex(index);
+ */
+
